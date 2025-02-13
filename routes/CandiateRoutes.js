c67e02b8bd14d928e1f38de394b4f4398451e26a
abhi <abhi.skamboj@gmail.com>
2025-02-13 23:35:46 +0530

    middleware authentication integrated

--- routes/CandiateRoutes.js
+++ routes/CandiateRoutes.js
@@ -1,16 +1,18 @@
 const express=require('express')
 const router=express.Router();
-const CandidateModel=require('../models/CandidateSchema')
-router.post('/postCandidateImage',async(req,res)=>{
-    try{
-        const {accountAddress,ImageName}=req.body;
-        const saveCandidate=await CandidateModel.create({
-            accountAddress:accountAddress,
-            ImageName:ImageName
-        })
-        res.status(200).json(saveCandidate)
-    }catch(er){
-        console.error(er)
-    }
+const CandidateModel=require('../models/CandidateSchema');
+const { authentication } = require('../middleware/authentication');
+router.post('/postCandidateImage',authentication,async(req,res)=>{
+    console.log("postcandidate")
+    // try{
+    //     const {accountAddress,ImageName}=req.body;
+    //     const saveCandidate=await CandidateModel.create({
+    //         accountAddress:accountAddress,
+    //         ImageName:ImageName
+    //     })
+    //     res.status(200).json(saveCandidate)
+    // }catch(er){
+    //     console.error(er)
+    // }
 })
 module.exports=router
